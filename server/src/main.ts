5715630993aa1d2e007cc7187934167bd453bbaa
ardhendu002 <ardhendupurnendu@gmail.com>
2026-06-25 18:39:54 +0530

    fixed-cors-2

--- server/src/main.ts
+++ server/src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
   app.enableCors({
   origin: [
     'http://localhost:3000',
-    'https://ruchi-performx.vercel.app',
+    'https://app.ruchiperformx.in',
   ],
   credentials: true,
 });
